90a212c6f8ce393695670795095d2a70f0a2c943
MarceloAvanzi <marcelodaniel@hotmail.com>
2024-02-12 21:14:09 -0300

    changing architecture

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Home: React.FC = () => {
+    return <div>
+        <h1>Home</h1>
+        <a>
+            <h2>Cha de casa nueva</h2>
+        </a>
+    </div>;
+}
+
+export default Home;
