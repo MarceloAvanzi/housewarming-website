88aa8ee2166fe60b9d5942507a75e6fd8f807161
MarceloAvanzi <marcelodaniel@hotmail.com>
2024-03-25 11:03:08 -0300

    change images folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ export default {
     },
     extend: {
       backgroundImage: {
-        'house': "url('src/assets/images/backgrounds/housebg.jpg')",
+        'house': "url('./src/assets/images/backgrounds/housebg.jpg')",
       },
       fontFamily: {
         'montez': ['Montez', 'cursive'],
