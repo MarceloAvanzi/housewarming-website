545bd03faa3b34d58dfef995c976ae1590dbd354
MarceloAvanzi <marcelodaniel@hotmail.com>
2024-04-17 20:39:07 -0300

    changing images size and cards

--- src/components/MainLayout/index.tsx
+++ src/components/MainLayout/index.tsx
@@ -4,7 +4,7 @@ import Header from '../../components/Header';
 const MainLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
     return (
         <>
-            <div className='min-h-screen min-y-screen bg-house bg-cover opacity-70'>
+            <div className='min-h-screen min-y-screen bg-house bg-center bg-cover'>
                 <Header />
                 <main className='flex-grow'>{children}</main>
             </div>
--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -22,13 +22,13 @@ const Products: React.FC<{ onClick: (product: Product) => void }> = ({ onClick }
 
     return (
         <div
-            className="flex items-center p-4 border border-gray-300 h-auto bg-white bg-opacity-80 rounded-xl hover:shadow-md hover:border-black cursor-pointer"
+            className="flex items-center p-4 border border-gray-300 h-auto bg-white bg-opacity-30 rounded-xl hover:shadow-md hover:border-black cursor-pointer"
             onClick={handleClick}
         >
             <img src={product.image} alt={product.name} className="mobile:w-24 lg:w-auto h-auto mr-4 rounded-lg" />
             <div className="flex-grow">
-                <p className="text-5xl font-semibold">{product.name}</p>
-                <p className="text-3xl text-gray-600">${product.price}</p>
+                <p className="lg:text-5xl mobile:text-xl font-semibold">{product.name}</p>
+                {/* <p className="text-3xl text-gray-600">${product.price}</p> */}
             </div>
         </div>
     );
