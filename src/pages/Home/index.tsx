8af34d437bb8c2c9901d528a88fde01eca92d056
Marcelo Avanzi <marcelodaniel@hotmail.com>
2024-07-16 21:10:30 -0300

    add motion to card description

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,24 +9,19 @@ const Home: React.FC = () => {
                 <div className="mx-auto max-w-screen">
                     <div className="p-8 overflow-y-scroll no-scrollbar" style={{ maxHeight: '75vh', overflowY: 'auto' }}>
                         <TextSection
-                            title="🏠 Gabi e Marcelo"
+                            title="🏠 Gabriela e Marcelo"
                             content="Estamos muito felizes em compartilhar a conquista do nosso novo lar com vocês! Gostaríamos de convidar todos os familiares para o nosso Chá de Casa Nova e comemorarmos juntos."
                             direction="left"
                         />
                         <TextSection
                             title="🍖 Como Funciona?"
-                            content="Vamos preparar um delicioso churrasco para todos. Contamos com a sua presença e se precisar de sugestões, veja a página de Lista de Presentes."
+                            content="Vamos preparar um delicioso churrasco para todos. Gostaríamos da sua presença conosco nesse momento especial! Para nos ajudar a construir esse sonho, te convidamos para ver nossa lista de presentes mas caso prefira pode nos presentear da forma que achar melhor"
                             direction="right"
                         />
                         <TextSection
                             title="📍 Localização"
                             content="Condomínio Parque dos Manacás às 15:00 - R Doutor Motta Junior, n° 2450"
                             direction="left"
-                        />
-                        <TextSection
-                            title="Por que Participar?"
-                            content="É uma ótima oportunidade para desfrutar de boa companhia, saborear uma belo churrasco e celebrar esse novo começo ao nosso lado!"
-                            direction="right"
                         />
                     </div>
                 </div>
