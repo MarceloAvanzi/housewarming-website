e88b8d35f5bac32acde77c7f757f4ea6a6524108
Marcelo Avanzi <marcelodaniel@hotmail.com>
2024-07-18 19:38:06 -0300

    add pics of owned list

--- src/data/ownedProducts.ts
+++ src/data/ownedProducts.ts
@@ -1,12 +1,11 @@
 const ownedProducts = [
-    { id: 1, name: 'Liquidificador', imagePath: '' },
-    { id: 2, name: 'Geladeira', imagePath: '' },
-    { id: 3, name: 'Cama Box', imagePath: '' },
-    { id: 4, name: 'Sanduicheira', imagePath: '' },
-    { id: 5, name: 'Mesa', imagePath: '' },
-    { id: 6, name: 'Aspirador', imagePath: '' },
+    { id: 1, name: 'Liquidificador', imagePath: 'liquidificador.jpg' },
+    { id: 2, name: 'Geladeira', imagePath: 'geladeira.jpg' },
+    { id: 3, name: 'Cama Box', imagePath: 'camabox.jpg' },
+    { id: 4, name: 'Sanduicheira', imagePath: 'sanduicheira.jpg' },
+    { id: 5, name: 'Mesa', imagePath: 'mesa.jpg' },
+    { id: 6, name: 'Aspirador', imagePath: 'aspirador.jpg' },
     { id: 7, name: 'Chuveiro', imagePath: 'chuveiro.jpg'},
-
 ];
 
 export default ownedProducts;
