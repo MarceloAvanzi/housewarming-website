6c0e0b1ec0c5523f26cd1f21fd8cc110b32806e4
Marcelo Avanzi <marcelodaniel@hotmail.com>
2024-07-23 20:12:37 -0300

    add message link

--- src/components/ConfirmPresence/index.tsx
+++ src/components/ConfirmPresence/index.tsx
@@ -4,7 +4,11 @@ import '../../static/ConfirmPresence.css';
 const ConfirmPresence: React.FC = () => {
     return (
         <div className='neon-button-container'>
-            <button className='neon-button'>Confirmar Presença</button>
+            <a href='https://wa.me/5541988353007?text=Confirmo%20presença%20no%20chá%20de%20casa%20nova!!' target='_blank' rel='noopener noreferrer'>
+                <button className='neon-button'>
+                    Confirmar Presença
+                </button>
+            </a>
         </div>
     );
 }
