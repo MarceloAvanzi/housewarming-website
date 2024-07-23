8cae796527b3a29ec4edecdf889b054e79b2ac72
Marcelo Avanzi <marcelodaniel@hotmail.com>
2024-07-23 20:18:43 -0300

    change number

--- src/components/ConfirmPresence/index.tsx
+++ src/components/ConfirmPresence/index.tsx
@@ -4,7 +4,7 @@ import '../../static/ConfirmPresence.css';
 const ConfirmPresence: React.FC = () => {
     return (
         <div className='neon-button-container'>
-            <a href='https://wa.me/5541988353007?text=Confirmo%20presença%20no%20chá%20de%20casa%20nova!!%20✨🏠' target='_blank' rel='noopener noreferrer'>
+            <a href='https://wa.me/5541996416590?text=Confirmo%20presença%20no%20chá%20de%20casa%20nova!!%20✨🏠' target='_blank' rel='noopener noreferrer'>
                 <button className='neon-button'>
                     Confirmar Presença
                 </button>
